66c417d7ba857f098ca4eb6cd162b1ac139c7c7f
Martijn <404113@student.fontys.nl>
2019-10-31 12:50:31 +0100

    Made loginPage and added homePage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div class="row">
+        <div id="Box">
+          <div class="form-group row">
+            <label class="col-sm-3 col-form-label">Username</label>
+            <div class="col-sm-9">
+              <input class="form-control"></input>
+            </div>
+          </div>
+          <div class="form-group row">
+            <label class=" col-sm-3 col-form-label">Password</label>
+            <div class="col-sm-9">
+              <input class="form-control"></input>
+            </div>
+          </div>
+          <Link to="/src/homePage.js"> Open Ananas </Link>
+          <button type="button" class="btn btn-warning btn-block btn-sm">
+            Open Ananas
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
