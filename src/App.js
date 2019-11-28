de35b6617dcd373410a2fd1ac465712fd62fffd2
Alfar414 <AS.cool@live.nl>
2019-11-28 12:46:20 +0100

    fix history

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import Nav from "./Components/Nav";
-import History from "./Components/History";
+// import History from "./Components/History";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -18,7 +18,7 @@ function App() {
               <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/Home" component={About} />
-                <Route path="/History" component={History} />
+                {/* <Route path="/History" component={History} /> */}
               </Switch>
             </div>
           </div>
